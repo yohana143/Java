cd66332f63ea73029da3067a6cceb3206b68e467
Yohana Erukala <yohana8896@gmail.com>
2021-12-15 14:01:42 +0530

    Add files via upload

---
+++ java_02_literals.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class java_02_literals {
+    public static void main(String[] args) {
+        int age=21;
+        byte height=5;
+        float weight=58.5f;
+        char ch='A';
+        long agedino=65416841651574l; //or L
+        double d=4.65D;  //D is also not reqiured
+        boolean a=true;
+        String a1="Yohana";
+        System.out.println(a1);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(agedino);
+    }
+}
